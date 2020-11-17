b054e430665d3a4ab139c73c7d6be60aea5c2b57
edrichmkhasi <edrich@thedigitalacademy.co.za>
2020-11-17 02:30:36 +0200

    The navbar of the amazon clone is implemented :)

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import Home from './components/home/Home';
 function App() {
   return (
     <div className="app">
-     <h1>Amazon Clone app</h1>
+   
      {/* Header Component */}
      <Header />
      {/* Home Component */}
-     <Home />
+     
     </div>
   );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 function Header() {
-   return (
-      <div>
-         <p>I'm Header Component</p>
+  return (
+    <div className='header'>
+      <img
+        className='header_logo'
+        src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
+        alt='Amazon Logo'
+      />
+
+      <div className='header_search'>
+        <input className='header_searchInput' type='text' />
+        <SearchIcon className='header_searchIcon' />
       </div>
-   )
+
+      <div className='header_nav'>
+        <div className='header_option'>
+          <span className='header_optionOne'>Hello </span>
+          <span className='header_optionTwo'>Guest</span>
+        </div>
+
+        <div className='header_option'>
+          <span className='header_optionOne'>Returns </span>
+          <span className='header_optionTwo'>& Orders</span>
+        </div>
+
+        <div className='header_option'>
+          <span className='header_optionOne'>Your </span>
+          <span className='header_optionTwo'>Prime</span>
+        </div>
+
+        <div className='header_optionBasket'>
+           <ShoppingBasketIcon />
+           <span className='header_optionTwo header_basketCount'>0</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
